2e02f3d70730a4475d40372c6a1a068f3bd41564
dmdghto <dmdghto@naver.com>
2019-03-17 22:20:11 +0900

    solve notificationchannel crash

--- app/src/main/java/com/example/uploadphoto/Constants.java
+++ app/src/main/java/com/example/uploadphoto/Constants.java
@@ -1,6 +1,6 @@
 package com.example.uploadphoto;
 
 public class Constants {
-    public static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
-    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+    public static final String UPLOAD_URL = "http://jhjeong713.iptime.org/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://jhjeong713.iptime.org/AndroidImageUpload/getImages.php";
 }
